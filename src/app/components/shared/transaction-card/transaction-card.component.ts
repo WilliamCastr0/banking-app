a59812eba8140d3c84631549d0647df65e3a5bdc
William Castro <wmcjimenez@gmail.com>
2024-03-27 13:41:05 -0500

    feat: :sparkles: Add id & value types according to the API

--- src/app/components/shared/transaction-card/transaction-card.component.ts
+++ src/app/components/shared/transaction-card/transaction-card.component.ts
@@ -1,20 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-transaction-card',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, CommonModule],
   templateUrl: './transaction-card.component.html',
   styleUrl: './transaction-card.component.css',
 })
 export class TransactionCardComponent {
+  @Input() id: string = '';
   @Input() urlArrowTransaction: string = 'assets/icons/arrow-up.svg';
   @Input() titleTransaction: string = '';
-  @Input() dateTransaction: string = '';
-  @Input() valueTransaction: string = '';
+  @Input() dateTransaction: number = 0;
+  @Input() valueTransaction: number = 0;
   @Input() category: string = '';
-  @Input() urlDetails: string = '';
+  @Input() withHover: boolean = false;
 
   srcArrowTransaction: string = '';
 
