53d8e0157544c490c39978759766e7b1371c79c9
William Castro <wmcjimenez@gmail.com>
2024-03-27 02:04:51 -0500

    feat: :sparkles: Mock up dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import { MoneyCardComponent } from '../shared/money-card/money-card.component';
+import { AccountCardComponent } from '../shared/account-card/account-card.component';
+import { TransactionCardComponent } from '../shared/transaction-card/transaction-card.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MoneyCardComponent,
+    AccountCardComponent,
+    TransactionCardComponent,
+    RouterOutlet,
+    RouterLink,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
