575393c2760f43c48d2b73b93db1568c3f6a539e
William Castro <wmcjimenez@gmail.com>
2024-03-26 22:33:30 -0500

    feat: :sparkles: Add account card component

---
+++ src/app/components/shared/account-card/account-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-account-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account-card.component.html',
+  styleUrl: './account-card.component.css',
+})
+export class AccountCardComponent {
+  @Input() iconBackground: string = 'red';
+  @Input() iconUrl: string = '';
+  @Input() title: string = '';
+  @Input() value: string = '';
+  @Input() isForTransaction: boolean = false;
+  @Input() description: string = '';
+  @Input() withValue: boolean = false;
+}
