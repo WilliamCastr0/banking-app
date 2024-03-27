1c1a860602a2a8b5ae34bf9b8b533c3364e6adca
William Castro <wmcjimenez@gmail.com>
2024-03-27 13:40:26 -0500

    feat: :sparkles: Add transactions logic in transactions module

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { TransactionCardComponent } from '../shared/transaction-card/transaction-card.component';
+import { TransactionStore } from '../../transaction.store';
+import { Transaction } from '../../types/transaction';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [TransactionCardComponent, RouterLink, RouterOutlet],
+  imports: [TransactionCardComponent, CommonModule, RouterLink, RouterOutlet],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css',
 })
 export class TransactionsComponent {
-  pageTitle: string = '';
+  // pageTitle: string = '';
+  transactionList: Transaction[] = [];
+  transactionService: TransactionStore = inject(TransactionStore);
+  transactions$: Observable<Transaction[]> = this.store.state$;
 
-  constructor(public title: Title) {}
+  constructor(public title: Title, private store: TransactionStore) {}
 
-  ngOnInit() {
-    this.pageTitle = this.title.getTitle();
+  async ngOnInit() {
+    // this.pageTitle = this.title.getTitle();
+    try {
+      this.store.getAllTransactions();
+    } catch (error: unknown) {
+      alert('Something went wrong!');
+    } finally {
+    }
   }
 }
