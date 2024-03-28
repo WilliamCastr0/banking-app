2f76ac587c89aecb1407bc69eca01af378ed4f8e
William Castro <wmcjimenez@gmail.com>
2024-03-28 03:49:13 -0500

    feat: :heavy_plus_sign: Add ngx-toastr for notifications

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideToastr } from 'ngx-toastr';
 
 import routeConfig from './routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(HttpClientModule),
-    provideRouter(routeConfig)
-  ]
+    provideRouter(routeConfig),
+    provideAnimations(),
+    provideToastr(),
+  ],
 };
