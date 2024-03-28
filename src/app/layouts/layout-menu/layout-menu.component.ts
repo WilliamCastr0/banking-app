4cbae12ab78d5195c4ad9a25257143395462b7e5
William Castro <wmcjimenez@gmail.com>
2024-03-28 15:46:40 -0500

    feat: :sparkles: Add layout for the dashboard

---
+++ src/app/layouts/layout-menu/layout-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from '../../components/shared/header/header.component';
+import { MenuComponent } from '../../components/shared/menu/menu.component';
+
+@Component({
+  selector: 'app-layout-menu',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  templateUrl: './layout-menu.component.html',
+  styleUrl: './layout-menu.component.css',
+})
+export class LayoutMenuComponent {}
