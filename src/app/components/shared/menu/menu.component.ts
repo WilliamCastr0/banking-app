0d7a33a45b416adb8bfde5ab78d161a5d48ddfa2
William Castro <wmcjimenez@gmail.com>
2024-03-30 02:43:17 -0500

    feat: :iphone: Add mobile menu

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -5,6 +5,7 @@ import { HomeIconComponent } from '../icons/home-icon/home-icon.component';
 import { TransactionIconComponent } from '../icons/transaction-icon/transaction-icon.component';
 import { AccountIconComponent } from '../icons/account-icon/account-icon.component';
 import { SettingsIconComponent } from '../icons/settings-icon/settings-icon.component';
+import { AuthnService } from '../../../auth/authn.service';
 
 @Component({
   selector: 'app-menu',
@@ -20,4 +21,10 @@ import { SettingsIconComponent } from '../icons/settings-icon/settings-icon.comp
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  constructor(private authnService: AuthnService) {}
+
+  logout() {
+    this.authnService.logout();
+  }
+}
