11212820dde1c12342fa95512878ddac3e07fb09
William Castro <wmcjimenez@gmail.com>
2024-03-29 22:29:31 -0500

    feat: :sparkles: Add props in money card component

--- src/app/components/shared/money-card/money-card.component.ts
+++ src/app/components/shared/money-card/money-card.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { hideAccountPipe } from '../../../pipes/hideAccount.pipe';
 
 @Component({
   selector: 'app-money-card',
   standalone: true,
-  imports: [],
+  imports: [hideAccountPipe, CommonModule],
   templateUrl: './money-card.component.html',
   styleUrl: './money-card.component.css',
 })
 export class MoneyCardComponent {
   @Input() urlChipMoneyCard: string = 'assets/icons/chip-card-white.svg';
   @Input() urlMastercard: string = 'assets/icons/mastercard-white.svg';
+  @Input() balance: number = 0;
+  @Input() firstName: string = '';
+  @Input() account: string = '';
   srcChipMoneyCard: string = '';
   srcMastercard: string = '';
 
