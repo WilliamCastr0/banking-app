7a9895b53326d855f2168601cee4a0c26b8c5c43
William Castro <wmcjimenez@gmail.com>
2024-03-30 02:41:11 -0500

    feat: :sparkles: Add profile logic & template

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { UserStore } from '../../user/user.store';
+import User from '../../types/user';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  userService: UserStore = inject(UserStore);
+  user$: Observable<User> = this.userStore.state$;
+  userProfile: User = { firstName: '', lastName: '', accounts: [], email: '' };
+  userName: string = '';
 
+  constructor(private userStore: UserStore) {}
+
+  ngOnInit() {
+    this.userStore.getProfile();
+    this.user$.subscribe((user) => {
+      this.userProfile = user;
+      this.userName = this.userProfile.firstName.substring(0, 1);
+    });
+  }
 }
